30c85860d009f81480299b580d157c328c332b97
Mhamidi <hamidi.michael@outlook.fr>
2018-06-19 11:34:47 +0200

    ECF2_11h34
    BddContext

---
+++ MulhouseHabitat/Models/BdddContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MulhouseHabitat.Models
+{
+    public class BdddContext:DbContext
+    {
+        public DbSet<MHLogements> Logements { get; set; }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<BddContext>(null);
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BdddContext>());
+            base.OnModelCreating(modelBuilder);
+            //si la base de donnée change le modele change automatiquement en ajoutant ou enlevant les champs
+        }
+    }
+}
